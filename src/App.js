b373964581634602893bd4197a496e303d767bee
Shubham Raj <shubham14p3@gmail.com>
2020-08-28 02:17:57 +0530

    added src files to the repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          hello react.
+        </p>        
+    </div>
+  );
+}
+
+export default App;
