f0803d9f4fcc76a58d64f736a7c75b1184a5ccca
Shubham Raj <shubham14p3@gmail.com>
2020-08-29 06:18:07 +0530

    added missing styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import{BrowserRouter as Router,Route} from 'react-router-dom';
 import Home from './components/Home';
 import QuizInstructions from './components/quiz/QuizInstructions';
+import Play from './components/quiz/Play';
+
 function App() {
   return (
     <Router>
     <Route path="/" exact component={Home}/>
     <Route path="/play/instructions" exact component={QuizInstructions}/>
+    <Route path="/play/Quiz" exact component={Play}/>
 </Router>
   );
 }
--- src/components/quiz/Play.js
+++ src/components/quiz/Play.js
@@ -1,6 +1,31 @@
 import React from 'react';
-class Play extends React.Component{
 
+class Play extends React.Component{
+constructor(props){
+    super(props);
+    this.state={
+        counter:0
+    };
+}
+
+increaseCount=()=>{
+    this.setState({
+        counter:5
+        });
+};
+
+
+render(){
+    return(
+        <div>
+            <p>Counter: {this.state.counter}</p>
+           <button onClick={this.increaseCount}>Click me</button>
+    
+        </div>
+
+            );
+       
+}
 } 
 
 export default Play;
