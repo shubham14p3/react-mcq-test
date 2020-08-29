7851a934057be786f5bbff4eeb348d316e198c1d
Shubham Raj <shubham14p3@gmail.com>
2020-08-29 06:07:04 +0530

    added App finctions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import{BrowserRouter as Router,Route} from 'react-router-dom';
 import Home from './components/Home';
+import QuizInstructions from './components/quiz/QuizInstructions';
 function App() {
   return (
     <Router>
-    <Route path="/" component={Home}/>
+    <Route path="/" exact component={Home}/>
+    <Route path="/play/instructions" exact component={QuizInstructions}/>
 </Router>
   );
 }
