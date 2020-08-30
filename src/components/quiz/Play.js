05599083d807dd95efeb329bdc15e2ad017ff974
Shubham Raj <shubham14p3@gmail.com>
2020-08-30 22:41:13 +0530

    added logic for question to be dinamic

--- src/components/quiz/Play.js
+++ src/components/quiz/Play.js
@@ -1,28 +1,60 @@
-import React, { Component, Fragment } from "react";
-import { Link } from "react-router-dom";
-import { Helmet } from "react-helmet";
+import React, { Component, Fragment } from 'react';
+import { Helmet } from 'react-helmet';
+import questions from '../../questions.json';
+import isEmpty from '../../utils/is-empty';
 
 class Play extends Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = {
-  //       counter: 0,
-  //     };
-  //   }
+    constructor(props) {
+      super(props);
+      this.state = {
+        questions,
+        currentQuestion: {},
+        nextQuestion: {},
+        previousQuestion: {},
+        answer: '',
+        numberOfQuestions: 0,
+        numberOfAnsweredQuestions: 0,
+        currentQuestionIndex: 0,
+        score: 0,
+        correctAnswers: 0,
+        wrongAnswers: 0,
+        hints: 5,
+        fiftyFifty: 2,
+        usedFiftyFifty: false,
+        time:{},
+      };
+    }
 
-  //   increaseCount = () => {
-  //     this.setState({
-  //       counter: 5,
-  //     });
-  //   };
+    componentDidMount(){
+      const { questions, currentQuestion, nextQuestion, previousQuestion } = this.state;
+        this.displayQuestions(questions, currentQuestion, nextQuestion, previousQuestion);
+      }
+      displayQuestions = (questions = this.state.questions, currentQuestion, nextQuestion, previousQuestion) => {
+        let { currentQuestionIndex } = this.state;   
+        if (!isEmpty(this.state.questions)) {
+            questions = this.state.questions;
+            currentQuestion = questions[currentQuestionIndex];
+            nextQuestion = questions[currentQuestionIndex + 1];
+            previousQuestion = questions[currentQuestionIndex - 1];
+            const answer = currentQuestion.answer;
+            this.setState({
+                currentQuestion,
+                nextQuestion,
+                previousQuestion,
+                numberOfQuestions: questions.length,
+                answer,
+        });
+      }
+    };
 
   render() {
+    const{currentQuestion}=this.state;
     return (
       <Fragment>
         <Helmet>
           <title>Quiz page</title>
         </Helmet>
-        <div className="questions">
+        <div className="question">
             <h2>Quiz Mode</h2>
           <div className="lifeline-container">
             <p>
@@ -44,14 +76,14 @@ class Play extends Component {
                   </span></span>
               </p>
           </div>
-          <h5>Google was Founder in which year?</h5>
+           <h5>{currentQuestion.question}</h5>
           <div className="options-containers">
-            <p className="options">1996</p>
-            <p className="options">1997</p>
+            <p className="options">{currentQuestion.optionA}</p>
+            <p className="options">{currentQuestion.optionB}</p>
           </div>
           <div className="options-containers">
-            <p className="options">1998</p>
-            <p className="options">2000</p>
+            <p className="options">{currentQuestion.optionC}</p>
+            <p className="options">{currentQuestion.optionD}</p>
           </div>
           <div className="button-container">
             <button>Previous</button>
--- src/utils/is-empty.js
+++ src/utils/is-empty.js
@@ -1,7 +1,7 @@
-const isEmpty=(value)=>
-value===undefined||
-value ==null ||
-(typeof valuue == 'object' && Object.keys(value).lenght===0)||
-(typeof valuue == 'string' && Object.trim().lenght===0));
+const isEmpty = (value) => 
+    value === undefined ||
+    value == null ||
+    (typeof value === 'object' && Object.keys(value).length === 0) ||
+    (typeof value === 'string' && value.trim().length === 0);
 
 export default isEmpty;
