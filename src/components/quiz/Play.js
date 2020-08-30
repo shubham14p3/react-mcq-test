d0f8928ab6db6c8711f75d70097453bd8e216a9b
Shubham Raj <shubham14p3@gmail.com>
2020-08-31 02:03:12 +0530

    added option logics

--- src/components/quiz/Play.js
+++ src/components/quiz/Play.js
@@ -1,61 +1,111 @@
-import React, { Component, Fragment } from 'react';
-import { Helmet } from 'react-helmet';
-import questions from '../../questions.json';
-import isEmpty from '../../utils/is-empty';
+import React, { Component, Fragment } from "react";
+import { Helmet } from "react-helmet";
+import M from "materialize-css";
+import questions from "../../questions.json";
+import isEmpty from "../../utils/is-empty";
 
 class Play extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        questions,
-        currentQuestion: {},
-        nextQuestion: {},
-        previousQuestion: {},
-        answer: '',
-        numberOfQuestions: 0,
-        numberOfAnsweredQuestions: 0,
-        currentQuestionIndex: 0,
-        score: 0,
-        correctAnswers: 0,
-        wrongAnswers: 0,
-        hints: 5,
-        fiftyFifty: 2,
-        usedFiftyFifty: false,
-        time:{},
-      };
-    }
-
-    componentDidMount(){
-      const { questions, currentQuestion, nextQuestion, previousQuestion } = this.state;
-        this.displayQuestions(questions, currentQuestion, nextQuestion, previousQuestion);
-      }
-      displayQuestions = (questions = this.state.questions, currentQuestion, nextQuestion, previousQuestion) => {
-        let { currentQuestionIndex } = this.state;   
-        if (!isEmpty(this.state.questions)) {
-            questions = this.state.questions;
-            currentQuestion = questions[currentQuestionIndex];
-            nextQuestion = questions[currentQuestionIndex + 1];
-            previousQuestion = questions[currentQuestionIndex - 1];
-            const answer = currentQuestion.answer;
-            this.setState({
-                currentQuestion,
-                nextQuestion,
-                previousQuestion,
-                numberOfQuestions: questions.length,
-                answer,
-        });
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions,
+      currentQuestion: {},
+      nextQuestion: {},
+      previousQuestion: {},
+      answer: "",
+      numberOfQuestions: 0,
+      numberOfAnsweredQuestions: 0,
+      currentQuestionIndex: 0,
+      score: 0,
+      correctAnswers: 0,
+      wrongAnswers: 0,
+      hints: 5,
+      fiftyFifty: 2,
+      usedFiftyFifty: false,
+      time: {},
     };
+  }
+
+  componentDidMount() {
+    const {
+      questions,
+      currentQuestion,
+      nextQuestion,
+      previousQuestion,
+    } = this.state;
+    this.displayQuestions(
+      questions,
+      currentQuestion,
+      nextQuestion,
+      previousQuestion
+    );
+  }
+  displayQuestions = (
+    questions = this.state.questions,
+    currentQuestion,
+    nextQuestion,
+    previousQuestion
+  ) => {
+    let { currentQuestionIndex } = this.state;
+    if (!isEmpty(this.state.questions)) {
+      questions = this.state.questions;
+      currentQuestion = questions[currentQuestionIndex];
+      nextQuestion = questions[currentQuestionIndex + 1];
+      previousQuestion = questions[currentQuestionIndex - 1];
+      const answer = currentQuestion.answer;
+      this.setState({
+        currentQuestion,
+        nextQuestion,
+        previousQuestion,
+        numberOfQuestions: questions.length,
+        answer,
+      });
+    }
+  };
+
+  handleOptionClick = (e) => {
+   if(e.target.innerHTML.toLowerCase() === this.state.answer.toLowerCase()){
+     this.correctAnswer();
+   }else{
+     this.wrongAnswer();
+   }
+  }
+  correctAnswer = () => {
+    M.toast({
+      html: "Correct Answer!",
+      classes: "toast-valid",
+      displayLength: 1500,
+    });
+    this.setState((prevState) => ({
+      score: prevState.score + 1,
+      correctAnswers: prevState.correctAnswers + 1,
+      currentQuestionIndex: prevState.currentQuestionIndex + 1,
+      numberOfAnsweredQuestions: prevState.numberOfAnsweredQuestions + 1,
+    }));
+  };
+  wrongAnswer = () => {
+    navigator.vibrate(1000);
+    M.toast({
+      html: "Wrong Answer!",
+      classes: "toast-invalid",
+      displayLength: 1500,
+    });
+    this.setState((prevState) => ({
+      wrongAnswers: prevState.wrongAnswers + 1,
+      currentQuestionIndex: prevState.currentQuestion + 1,
+      numberOfAnsweredQuestions: prevState.numberOfAnsweredQuestions,
+    }));
+  };
 
   render() {
-    const{currentQuestion}=this.state;
+    const { currentQuestion } = this.state;
     return (
       <Fragment>
         <Helmet>
           <title>Quiz Page</title>
         </Helmet>
         <div className="questions">
-            <h2>Quiz Mode</h2>
+          <h2>Quiz Mode</h2>
           <div className="lifeline-container">
             <p>
               <span className="mdi mdi-set-center mdi-24px lifeline-icon"></span>
@@ -65,24 +115,34 @@ class Play extends Component {
               <span className="mdi mdi-lightbulb-on-outline mdi-24px lifeline-icon"></span>
               <span className="lifeline"> 5</span>
             </p>
-          </div>    
+          </div>
           <div className="timer-container">
-              <p>
-                  <span className="left" style={{float:'left'}}>
-                      1 of 15
-                  </span>
-                  <span className="right">2:35 
-                  <span className="mdi mdi-clock-outline mdi-24px"></span>   </span>             
-              </p>
+            <p>
+              <span className="left" style={{ float: "left" }}>
+                1 of 15
+              </span>
+              <span className="right">
+                2:35
+                <span className="mdi mdi-clock-outline mdi-24px"></span>{" "}
+              </span>
+            </p>
           </div>
-           <h5>{currentQuestion.question}</h5>
+          <h5>{currentQuestion.question}</h5>
           <div className="options-container">
-            <p className="option">{currentQuestion.optionA}</p>
-            <p className="option">{currentQuestion.optionB}</p>
+            <p onClick={this.handleOptionClick} className="option">
+              {currentQuestion.optionA}
+            </p>
+            <p onClick={this.handleOptionClick} className="option">
+              {currentQuestion.optionB}
+            </p>
           </div>
           <div className="options-container">
-            <p className="option">{currentQuestion.optionC}</p>
-            <p className="option">{currentQuestion.optionD}</p>
+            <p onClick={this.handleOptionClick} className="option">
+              {currentQuestion.optionC}
+            </p>
+            <p onClick={this.handleOptionClick} className="option">
+              {currentQuestion.optionD}
+            </p>
           </div>
           <div className="button-container">
             <button>Previous</button>
